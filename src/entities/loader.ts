e4377dfb751a22ff5a6e606da29655a81b239a35
GhomKrosmonaute <camille.abella@hotmail.fr>
2023-07-27 16:58:09 +0200

    removed useless code in loader

--- src/entities/loader.ts
+++ src/entities/loader.ts
@@ -1,34 +1,20 @@
-import * as pixi from "pixi.js"
-import * as booyah from "@ghom/booyah"
 import * as pixiChip from "./pixiChip"
 import * as progressBar from "./progressBar"
-
-interface BaseRefinedResource {
-  name: string
-}
 
 export interface LoaderOptions<Raw, Refined> {
   source: readonly Raw[]
   transform: (raw: Raw) => Promise<Refined> | Refined
-  onLoaded?: (resources: Refined[]) => void
+  onLoaded: (resources: Refined[]) => void
 }
 
 export class Loader<Raw, Refined> extends pixiChip.Container {
-  private _resources!: Refined[]
   private _progressBar!: progressBar.ProgressBar
 
   constructor(private _options: LoaderOptions<Raw, Refined>) {
     super()
   }
 
-  /**
-   * @todo
-   *   - Show loading screen with a progress bar and the name of loaded resources.
-   *   - On loaded, terminate this entity
-   */
   protected _onActivate() {
-    this._resources = []
-
     this._progressBar = new progressBar.ProgressBar({
       startValue: 0,
       color: 0x247afd,
@@ -42,17 +28,18 @@ export class Loader<Raw, Refined> extends pixiChip.Container {
 
     this._activateChildChip(this._progressBar)
 
+    const resources: Refined[] = []
+
     const load = async () => {
       for (const raw of this._options.source) {
-        this._resources.push(await this._options.transform(raw))
-        this._progressBar.value =
-          this._resources.length / this._options.source.length
+        resources.push(await this._options.transform(raw))
+        this._progressBar.value = resources.length / this._options.source.length
       }
     }
 
     load()
       .then(() => {
-        this._options.onLoaded?.(this._resources)
+        this._options.onLoaded?.(resources)
         this.terminate()
       })
       .catch((error: Error) => {
